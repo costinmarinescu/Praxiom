70acc3e72b9737bc3bbad65613c8e51deef6dd64
costinmarinescu <87228701+costinmarinescu@users.noreply.github.com>
2025-10-31 18:02:00 +0100

    Update PraxiomHealth.h

--- src/utility/PraxiomHealth.h
+++ src/utility/PraxiomHealth.h
@@ -4,7 +4,6 @@
 
 namespace Pinetime {
   namespace Praxiom {
-    float CalculateRealtimeAdjustment(uint8_t heartRate, uint32_t steps);
     uint16_t ComputeAdjustedBioAge(uint16_t baseAgeTenths, uint8_t heartRate, uint32_t steps);
   }
 }
