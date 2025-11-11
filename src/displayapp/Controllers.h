5e8962bf30b4628675bf580c1c0327066fb8cdb9
costinmarinescu <87228701+costinmarinescu@users.noreply.github.com>
2025-11-11 13:15:26 +0100

    Add files via upload

--- src/displayapp/Controllers.h
+++ src/displayapp/Controllers.h
@@ -4,7 +4,6 @@ namespace Pinetime {
   namespace System {
     class SystemTask;
   }
-  
   namespace Controllers {
     class Battery;
     class Ble;
@@ -22,44 +21,44 @@ namespace Pinetime {
     class StopWatchController;
     class BrightnessController;
     class Timer;
-    class NimbleController;  // Forward declaration for nimbleController member
+    class NimbleController; // Forward declaration for nimbleController member
+    class PraxiomService;  // *** ADDED FOR PRAXIOM ***
   }
-  
+
   namespace Components {
     class LittleVgl;
   }
 
   namespace Applications {
     class DisplayApp;
-    
     struct AppControllers {
       // CRITICAL: Order and types MUST EXACTLY match DisplayApp.cpp lines 111-129!
       // DO NOT add or remove members without updating DisplayApp.cpp constructor
-      
-      const Controllers::Battery& batteryController;              // 1
-      const Controllers::Ble& bleController;                      // 2
-      Controllers::DateTime& dateTimeController;                  // 3
-      Controllers::NotificationManager& notificationManager;      // 4
-      Controllers::HeartRateController& heartRateController;      // 5
-      Controllers::Settings& settingsController;                  // 6
-      Controllers::MotorController& motorController;              // 7
-      Controllers::MotionController& motionController;            // 8
-      Controllers::StopWatchController& stopWatchController;      // 9
-      Controllers::AlarmController& alarmController;              // 10
-      Controllers::BrightnessController& brightnessController;    // 11
-      Pinetime::System::SystemTask* systemTask;                   // 12 - set by Register()
-      Controllers::FS& filesystem;                                // 13
-      Controllers::Timer& timer;                                  // 14
-      Controllers::MusicService* musicService;                    // 15 - set by Register()
-      DisplayApp* displayApp;                                     // 16
-      Pinetime::Components::LittleVgl& lvgl;                      // 17
-      Controllers::NavigationService* navigationService;          // 18 - set by Register()
-      Controllers::SimpleWeatherService* weatherController;       // 19 - set by Register()
+      const Controllers::Battery& batteryController;           // 1
+      const Controllers::Ble& bleController;                   // 2
+      Controllers::DateTime& dateTimeController;               // 3
+      Controllers::NotificationManager& notificationManager;   // 4
+      Controllers::HeartRateController& heartRateController;   // 5
+      Controllers::Settings& settingsController;               // 6
+      Controllers::MotorController& motorController;           // 7
+      Controllers::MotionController& motionController;         // 8
+      Controllers::StopWatchController& stopWatchController;   // 9
+      Controllers::AlarmController& alarmController;           // 10
+      Controllers::BrightnessController& brightnessController; // 11
+      Pinetime::System::SystemTask* systemTask;                // 12 - set by Register()
+      Controllers::FS& filesystem;                             // 13
+      Controllers::Timer& timer;                               // 14
+      Controllers::MusicService* musicService;                 // 15 - set by Register()
+      DisplayApp* displayApp;                                  // 16
+      Pinetime::Components::LittleVgl& lvgl;                   // 17
+      Controllers::NavigationService* navigationService;       // 18 - set by Register()
+      Controllers::SimpleWeatherService* weatherController;    // 19 - set by Register()
       
       // Additional members needed by WatchFaceDigital and other screens:
       // These are POINTERS because they're set via Register() after construction
-      Controllers::SimpleWeatherService* weatherService;          // Pointer - dereferenced in WatchFaceDigital
-      Controllers::NimbleController* nimbleController;            // Pointer - used in WatchFaceDigital
+      Controllers::SimpleWeatherService* weatherService;       // Pointer - dereferenced in WatchFaceDigital
+      Controllers::NimbleController* nimbleController;         // Pointer - used in WatchFaceDigital
+      Controllers::PraxiomService* praxiomService;            // *** ADDED FOR PRAXIOM *** Pointer - set via Register()
     };
   }
 }
